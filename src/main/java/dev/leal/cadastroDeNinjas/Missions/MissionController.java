450fd72ac610ae383dbecabc191b56b400a903ed
Gabriel Leal <gabriel.leal@ifood.com.br>
2025-04-17 09:07:43 -0300

    feat: create routes mock in mission controller

---
+++ src/main/java/dev/leal/cadastroDeNinjas/Missions/MissionController.java
@@ -0,0 +1,27 @@
+package dev.leal.cadastroDeNinjas.Missions;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("mission")
+public class MissionController {
+
+    @PostMapping("/create")
+    public String create(){
+        return "criar missao";
+    }
+    @GetMapping("/findAll")
+    public String fingAll(){
+        return "listar missoes";
+    }
+
+    @PutMapping("/updateID")
+    public String update(){
+        return "Atualizar missão";
+    }
+
+    @DeleteMapping("/delete")
+    public String delete(){
+        return "apagar missão";
+    }
+}
