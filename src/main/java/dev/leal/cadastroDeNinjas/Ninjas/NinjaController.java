0ae9441a0465290a36d0980d55721469163bb930
Gabriel Leal <gabriel.leal@ifood.com.br>
2025-04-17 08:56:34 -0300

    feat: update controller with mock routes

--- src/main/java/dev/leal/cadastroDeNinjas/Ninjas/NinjaController.java
+++ src/main/java/dev/leal/cadastroDeNinjas/Ninjas/NinjaController.java
@@ -1,8 +1,6 @@
 package dev.leal.cadastroDeNinjas.Ninjas;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping
@@ -11,5 +9,28 @@ public class NinjaController {
     @GetMapping("/welcome")
     public String welcome(){
         return "This is the first message of project java10x";
+    }
+
+    @PostMapping("/create")
+    public String create(){
+        return "Ninja criado";
+    }
+    @GetMapping("/findAll")
+    public String fingAll(){
+        return "listar ninjas";
+    }
+    @GetMapping("/findAllID")
+    public String fingById(){
+        return "listar ninjas por id";
+    }
+
+    @PutMapping("/updateID")
+    public String update(){
+        return "Atualizar ninja";
+    }
+
+    @DeleteMapping("/delete")
+    public String delete(){
+        return "apagar ninja";
     }
 }
