46b2f3e23bece44858c38eddbb741536847f96b9
Gabriel Leal <gabriel.leal@ifood.com.br>
2025-04-16 21:43:54 -0300

    chore: add credentials to bd

--- src/main/java/dev/leal/cadastroDeNinjas/Ninjas/NinjaRepository.java
+++ src/main/java/dev/leal/cadastroDeNinjas/Ninjas/NinjaRepository.java
@@ -1,4 +1,6 @@
 package dev.leal.cadastroDeNinjas.Ninjas;
 
-public class NinjaRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface NinjaRepository extends JpaRepository<NinjaModel, Long> {
 }
