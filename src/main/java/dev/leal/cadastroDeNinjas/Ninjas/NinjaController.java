c30fe35acc2a441e6aebe9738920b16265f9687b
Gabriel Leal <gabriellealttrj@gmail.com>
2025-04-30 21:03:52 -0300

    chore: create method save ninja

--- src/main/java/dev/leal/cadastroDeNinjas/Ninjas/NinjaController.java
+++ src/main/java/dev/leal/cadastroDeNinjas/Ninjas/NinjaController.java
@@ -1,20 +1,30 @@
 package dev.leal.cadastroDeNinjas.Ninjas;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/ninja")
 public class NinjaController {
 
+    private final NinjaService service;
+
+    NinjaController(NinjaService service){
+        this.service = service;
+    }
+
     @GetMapping("/welcome")
     public String welcome(){
         return "This is the first message of project java10x";
     }
 
-    @PostMapping("/create")
-    public String create(){
-        return "Ninja criado";
+    @PostMapping(value = "/create", produces = "application/json")
+    public ResponseEntity<NinjaModel> create(@RequestBody NinjaModel ninja){
+        final NinjaModel newNinja = service.createNinja(ninja);
+        return ResponseEntity.ok(newNinja);
     }
+
     @GetMapping("/findAll")
     public String fingAll(){
         return "listar ninjas";
--- src/main/java/dev/leal/cadastroDeNinjas/Ninjas/NinjaService.java
+++ src/main/java/dev/leal/cadastroDeNinjas/Ninjas/NinjaService.java
@@ -1,4 +1,18 @@
 package dev.leal.cadastroDeNinjas.Ninjas;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class NinjaService {
+
+    private final NinjaRepository repository;
+
+    NinjaService(NinjaRepository repository){
+        this.repository = repository;
+    }
+
+    NinjaModel createNinja(NinjaModel ninja){
+        return repository.save(ninja);
+    }
+
 }
