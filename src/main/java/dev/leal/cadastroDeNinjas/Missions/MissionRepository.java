b307fbaf3787eccba6b8ac69b374ed95b27acda6
Gabriel Leal <gabriellealttrj@gmail.com>
2025-05-01 22:50:57 -0300

    feat: create mission repository

---
+++ src/main/java/dev/leal/cadastroDeNinjas/Missions/MissionRepository.java
@@ -0,0 +1,6 @@
+package dev.leal.cadastroDeNinjas.Missions;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MissionRepository extends JpaRepository<MissionModel, Long> {
+}
